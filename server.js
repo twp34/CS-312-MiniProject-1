26c92d81194c014b9d0db28b8797d709d7a95b93
twp34 <twp34@nau.edu>
2025-09-15 12:27:47 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Temporary array for blog posts
+let posts = [];
+
+// Home route
+app.get("/", (req, res) => {
+    res.render("index", { posts });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.post("/new", (req, res) => {
+    const { title, content } = req.body;
+    const newPost = {
+        title,
+        content,
+        date: new Date()
+    };
+    posts.push(newPost);
+    res.redirect("/");
+
+});
+
+app.get("/delete/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    if (!isNaN(id) && posts[id]) {
+        posts.splice(id, 1); // remove post at that index
+    }
+    res.redirect("/");
+});
+
+app.get("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts[id];
+    if (post) {
+        res.render("edit", { post, id });
+    } else {
+        res.redirect("/");
+    }
+});
+
+app.post("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    if (!isNaN(id) && posts[id]) {
+        posts[id].title = req.body.title;
+        posts[id].content = req.body.content;
+        posts[id].date = new Date(); // optional: update timestamp
+    }
+    res.redirect("/");
+});
