f07a82ab15a28f741399fc747386a15b12c93ea8
twp34 <twp34@nau.edu>
2025-09-15 15:59:16 -0700

    Initial commit

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const app = express();
-app.set("view engine", "ejs");
+const express = require("express"); //lets use use express
+const app = express(); //creates app
+app.set("view engine", "ejs"); //use expressjs
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //required to read post requests
 
-let posts = [];
+let posts = []; //variable to store posts, viewed on main page of site
 
-app.get("/", (req, res) => res.render("index", { posts }));
+app.get("/", (req, res) => res.render("index", { posts })); //renders index.ejs (main blog page)
 
-app.post("/new", (req, res) => {
+app.post("/new", (req, res) => { //actually captures data and creates post from input data once button is pressed, also creates an ID for that post
   posts.push({
     author: req.body.author,
     title: req.body.title,
     content: req.body.content,
     date: new Date()
   });
-  res.redirect("/");
+  res.redirect("/"); //refreshes page
 });
 
 
-app.get("/delete/:id", (req, res) => {
+app.get("/delete/:id", (req, res) => { //deletes post when button is pressed based on ID (res)
   posts.splice(req.params.id, 1);
   res.redirect("/");
 });
 
-app.get("/edit/:id", (req, res) => {
-  res.render("edit", { post: posts[req.params.id], id: req.params.id });
-});
+//app.get("/edit/:id", (req, res) => { 
+//  res.render("edit", { post: posts[req.params.id], id: req.params.id });
+//});
 
 app.post("/edit/:id", (req, res) => {
   posts[req.params.id] = { 
