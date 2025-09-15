0635f5f7fc8893de89d68d8c173c4c320b906ba7
twp34 <twp34@nau.edu>
2025-09-15 14:48:43 -0700

    Initial commit

--- server.js
+++ server.js
@@ -1,30 +1,59 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+// Temporary array for blog posts
 let posts = [];
 
-app.get("/", (req, res) => res.render("index", { posts }));
+// Home route
+app.get("/", (req, res) => {
+    res.render("index", { posts });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
 app.post("/new", (req, res) => {
-  posts.push({ title: req.body.title, content: req.body.content, date: new Date() });
-  res.redirect("/");
+    const { title, content } = req.body;
+    const newPost = {
+        title,
+        content,
+        date: new Date()
+    };
+    posts.push(newPost);
+    res.redirect("/");
+
 });
 
 app.get("/delete/:id", (req, res) => {
-  posts.splice(req.params.id, 1);
-  res.redirect("/");
+    const id = parseInt(req.params.id);
+    if (!isNaN(id) && posts[id]) {
+        posts.splice(id, 1); // remove post at that index
+    }
+    res.redirect("/");
 });
 
 app.get("/edit/:id", (req, res) => {
-  res.render("edit", { post: posts[req.params.id], id: req.params.id });
+    const id = parseInt(req.params.id);
+    const post = posts[id];
+    if (post) {
+        res.render("edit", { post, id });
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.post("/edit/:id", (req, res) => {
-  posts[req.params.id] = { title: req.body.title, content: req.body.content, date: new Date() };
-  res.redirect("/");
+    const id = parseInt(req.params.id);
+    if (!isNaN(id) && posts[id]) {
+        posts[id].title = req.body.title;
+        posts[id].content = req.body.content;
+        posts[id].date = new Date(); // optional: update timestamp
+    }
+    res.redirect("/");
 });
-
-app.listen(3000, () => console.log("Running on http://localhost:3000"));
