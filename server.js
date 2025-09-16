18dfd563e04b31c936e599b8d39cdc8c182eec7e
twp34 <twp34@nau.edu>
2025-09-15 19:52:57 -0700

    Initial commit

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ app.set("view engine", "ejs"); //use expressjs
 
 app.use(express.urlencoded({ extended: true })); //required to read POST requests
 
-let blogPosts = []; //array to store posts, viewed on main page of site (which for me is index.ejs
+let posts = []; //array to store posts, viewed on main page of site (which for me is index.ejs
 
-app.get("/", (req, res) => res.render("index", { blogPosts })); //renders index.ejs (main blog page)
+app.get("/", (req, res) => res.render("index", { posts })); //renders index.ejs (main blog page)
 
 app.post("/newPost", (req, res) => { //actually captures data and creates post from input data once button is pressed, also creates an ID for that post
-	blogPosts.push({ author: req.body.author,
+	posts.push({ author: req.body.author,
 	title: req.body.title,
 	content: req.body.content,
 	date: new Date()});
@@ -17,15 +17,15 @@ app.post("/newPost", (req, res) => { //actually captures data and creates post f
 });
 
 app.get("/delete/:id", (req, res) => { //deletes post when button is pressed based on ID
-	blogPosts.splice(req.params.id, 1);
+	posts.splice(req.params.id, 1);
 	res.redirect("/");
 });
 
 app.get("/edit/:id", (req, res) => { //shows the edit form for the one who clicked the edit button
-	res.render("edit", { post: blogPosts[req.params.id], id: req.params.id });
+	res.render("edit", { post: posts[req.params.id], id: req.params.id });
 });
 app.post("/edit/:id", (req, res) => { //actually handes the editting of the previous post, once the editor clicks save on new data
-	blogPosts[req.params.id] = { 
+	posts[req.params.id] = { 
 	title: req.body.title, 
 	content: req.body.content, 
 	author: req.body.author, 
